888f82f3ed8e9d033d7199ebe174f20b7bf5e257
Tianqi Zhu <tianqizhu@Tianqis-MacBook-Air.local>
2026-02-13 15:54:59 -0500

    Fix GitHub Pages deploy (Astro)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// export default defineConfig({
+//     site: 'https://clay-astro-theme.netlify.app',
+//     integrations: [sitemap()],
+// });
+
 export default defineConfig({
-    site: 'https://clay-astro-theme.netlify.app',
-    integrations: [sitemap()],
+  site: "https://zhutianqi.github.io",
+  base: "/clay-astro-theme",
 });
